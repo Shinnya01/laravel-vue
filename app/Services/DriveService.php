fea1e6af3c8bba1f6b541040adc1affd78e84b63
Shinnya01 <carreon.carll@gmail.com>
2026-01-12 10:02:03 +0800

    feat: added services

---
+++ app/Services/DriveService.php
@@ -0,0 +1,14 @@
+<?php 
+
+ namespace App\Services;
+
+use App\Models\Folder;
+
+class DriveService
+{
+    public function createFolder(array $data): Folder
+    {
+        dd('service working');
+    }
+}
+?>
